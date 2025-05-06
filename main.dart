751e6eb6452aaf0dcf396d1f586618442d87694c
Mohammad Alhanini <mohammad1alhanini@gmail.com>
2025-05-06 18:21:48 +0300

    update

--- main.dart
+++ main.dart
@@ -21,4 +21,6 @@ void main() {
   DentalCare.bookAppointment(patient3, Doctor2, "28-5-2025", "3pm");
 
   DentalCare.ShowAllAppointment();
+
+  DentalCare.showAllDoctors();
 }
--- models/patient.dart
+++ models/patient.dart
@@ -3,10 +3,4 @@ class Patient {
   int phone_num;
 
   Patient({required this.name, required this.phone_num});
-
-  void DIsplayPatientInfo() {
-    print("The patient name $name");
-    print("The patient phone number $phone_num");
-    print("_____________________________");
-  }
 }
